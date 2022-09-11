cb1a38704c673611b28f8dc401d24ea920461127
Gabriel Costa <gabrielcostasilva100@gmail.com>
2022-09-10 21:35:39 -0300

    [#] metodos do repositório feito e adicionado no gameService

--- back-replay/src/repository/game-repository.js
+++ back-replay/src/repository/game-repository.js
@@ -1,30 +1,51 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { readFile, writeFile, readdir } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const filepath = join(currentDir, "./../database");
 
 class GameRepository {
-  constructor({ file }) {
-    this.file = file;
+  constructor({ dir }) {
+    this.file = dir;
   }
 
-  async _currentFile() {
-    return JSON.parse(await readFile(this.file));
+  async findFile(filename) {
+    const path = join(this.file, filename);
+    try {
+      const data = JSON.parse(await readFile(path));
+      return [path, data];
+    } catch (e) {
+      return await this.createJson(this.removeType(filename));
+    }
   }
 
-  async find() {
-    return this._currentFile();
+  async _currentDir() {
+    return JSON.parse(await readdir(this.file));
   }
 
-  async create(data) {
-    const currentFile = await this._currentFile();
-    currentFile.push(data);
+  async create(data, onto) {
+    const [currentFile, fileData] = await this.findFile(onto);
+    fileData.push(data);
 
-    return await writeFile(this.file, JSON.stringify(currentFile));
+    return await writeFile(currentFile, JSON.stringify(fileData));
   }
 
-  async update() {}
+  async createJson(id) {
+    const filepath = join(this.file, `${id}.json`);
+    const data = [];
+    await writeFile(filepath, JSON.stringify(data));
+    return [filepath, data];
+  }
+
+  removeType(filename) {
+    const name = filename.split(".")[0];
+    return name;
+  }
 }
 
 const gameRepository = new GameRepository({
-  file: "../database/data.json",
+  dir: filepath,
 });
 
 export default gameRepository;
--- back-replay/src/service/game-service.js
+++ back-replay/src/service/game-service.js
@@ -1,18 +1,25 @@
 import Move from "../entities/move.js";
+import gameRepository from "../repository/game-repository.js";
 
 class GameService {
-    constructor(move) {
-        this.move = move;
-    }
+  constructor(move, repository) {
+    this.move = move;
+    this.repository = repository;
+  }
 
-    create(item) {
-        return this.move.build(item);
-    }
+  create(item) {
+    return this.move.build(item);
+  }
 
-    save() {
-        console.log("Hi");
-        return new Move();
-    }
+  async save(item, onto) {
+    const data = await this.repository.create(item, onto);
+    return data;
+  }
+
+  async find(id) {
+    const data = await this.repository.findFile(id);
+    return data;
+  }
 }
 
-export default new GameService(Move);
+export default new GameService(Move, gameRepository);
